07bba95a8a861bd4eb8c13e6a7a5723f6378032a
@Sinha199 <anshu.resource@gmail.com>
2021-06-11 11:37:48 +0530

    upadte routes

---
+++ track-via-notion/src/routes/route.js
@@ -0,0 +1,40 @@
+const controller = require('../controllers/controllers')
+
+async function routes(app,opts){
+    //show all appplications
+    app.routes({
+        method:'GET',
+        url:'job/applications',
+        handler:controller.getApplications
+    });
+
+    //get specific application
+    app.routes({
+        method:'GET',
+        url:'job/applications/:id',
+        handler:controller.getApplication
+    });
+
+    //filter job application as per the company name
+    app.routes({
+        method:'GET',
+        url:'job/applications/filter',
+        handler:controller.filterApplications
+    })
+
+    //add job application
+    app.routes({
+        method:'POST',
+        url:'job/applications',
+        handler:controller.addApplication
+    })
+
+    //update application
+    app.routes({
+        method:'PATCH',
+        url:'job/applications/:id',
+        handler:controller.updateApplication
+    })
+}
+
+module.exports=routes
